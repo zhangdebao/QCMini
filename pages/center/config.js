4681f516b83d750dbce87991e1b60b376b679a37
zhangdebao <1127701327@qq.com>
2021-01-17 22:18:45 +0800

    feat(): 个人中心、搜索

---
+++ pages/center/config.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    text: '密码修改',
+    src: 'https://zdb-bucket.oss-cn-shanghai.aliyuncs.com/QCMini/password.webp',
+    myClass: 'my-class1'
+  },
+  {
+    text: '我的信息',
+    src: 'https://zdb-bucket.oss-cn-shanghai.aliyuncs.com/QCMini/info.webp',
+    myClass: 'my-class2'
+  }
+]
---
+++ pages/search/config.js
@@ -0,0 +1,64 @@
+export default [
+  {
+    text: '生产查询',
+    src: 'https://zdb-bucket.oss-cn-shanghai.aliyuncs.com/QCMini/contract-fill.webp',
+    myClass: 'my-class1'
+  },
+  {
+    text: '任务查询',
+    src: 'https://zdb-bucket.oss-cn-shanghai.aliyuncs.com/QCMini/contract-check.webp',
+    myClass: 'my-class2'
+  },
+  {
+    text: '工程汇总查询',
+    src: 'https://zdb-bucket.oss-cn-shanghai.aliyuncs.com/QCMini/account.webp',
+    myClass: 'my-class3'
+  },
+  {
+    text: '配方查询',
+    src: 'https://zdb-bucket.oss-cn-shanghai.aliyuncs.com/QCMini/supplier.webp',
+    myClass: 'my-class3'
+  },
+  {
+    text: '入库查询',
+    src: 'https://zdb-bucket.oss-cn-shanghai.aliyuncs.com/QCMini/order.webp',
+    myClass: 'my-class1'
+  },
+  {
+    text: '车辆排队',
+    src: 'https://zdb-bucket.oss-cn-shanghai.aliyuncs.com/QCMini/orderReview.webp',
+    myClass: 'my-class2'
+  },
+
+  {
+    text: '订单查询',
+    src: 'https://zdb-bucket.oss-cn-shanghai.aliyuncs.com/QCMini/drawUp.webp',
+    myClass: 'my-class2'
+  },
+  {
+    text: '简仓库存',
+    src: 'https://zdb-bucket.oss-cn-shanghai.aliyuncs.com/QCMini/recipeManagement.webp',
+    myClass: 'my-class3'
+  },
+  {
+    text: '待检列表',
+    src: 'https://zdb-bucket.oss-cn-shanghai.aliyuncs.com/QCMini/recipeReview.webp',
+    myClass: 'my-class1'
+  },
+
+  {
+    text: '回款控制',
+    src: 'https://zdb-bucket.oss-cn-shanghai.aliyuncs.com/QCMini/receivableControl.webp',
+    myClass: 'my-class1'
+  },
+  {
+    text: '扫描校验',
+    src: 'https://zdb-bucket.oss-cn-shanghai.aliyuncs.com/QCMini/scan.webp',
+    myClass: 'my-class3'
+  },
+  {
+    text: '车辆状态',
+    src: 'https://zdb-bucket.oss-cn-shanghai.aliyuncs.com/QCMini/vehicle%20state.webp',
+    myClass: 'my-class2'
+  }
+]
