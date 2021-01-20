6afa0eac8b720db3ab43504ca275182d3b09a163
zhangdebao <1127701327@qq.com>
2021-01-20 22:30:04 +0800

    feat(): 订单管理

---
+++ components/orderItem/orderItem.js
@@ -0,0 +1,37 @@
+// components/orderItem/orderItem.js
+Component({
+  options: {
+    styleIsolation: 'isolated'
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    marginTop: {
+      type: Number,
+      value: 26
+    },
+    label: {
+      type: String,
+      value: '添加'
+    },
+    background: {
+      type: String,
+      value: 'background: linear-gradient(124deg, #536AE0, #5E88F7);'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
